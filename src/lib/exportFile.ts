8255189473f57f40269c14b694c98cde56c07ca4
hexie <hexie.zhang@tuya.com>
2021-12-08 21:53:36 +0800

    feat: 支持嵌套词条导出，格式与多语言平台保持一致

--- src/lib/exportFile.ts
+++ src/lib/exportFile.ts
@@ -1,89 +1,141 @@
-import * as fs from 'fs-extra'
-import { exportExcelFile } from '../until/index'
+import * as fs from "fs-extra";
+import * as path from "path";
+import { exportExcelFile } from "../until/index";
 
-
+interface deepData {
+  data: Object | any;
+  langNumber: number;
+  parent: string;
+}
 export default class ExportFile {
   paths: string;
-  mode: string;
-  constructor () {
-    this.paths = ''
-    this.mode = 'root'
+  num: number;
+  Rows: string[][];
+  Header: Object[];
+  constructor() {
+    this.paths = "";
+    this.num = 0; // 词条索引
+    this.Rows = []; // 表格行数据
+    this.Header = [{ caption: "code", type: "string" }]; // 表格头
   }
 
-  init (agv:{mode: string, paths: string}) {
+  init(agv: { mode: string; paths: string }) {
     switch (agv.mode) {
-      case 'root':
-        this.paths = agv.paths
-        this.modeRoot()
+      case "root":
+        this.paths = agv.paths;
+        this.modeRoot();
         break;
-      case 'multiple':
-        this.modeMultiple()
+      case "multiple":
+        this.modeMultiple();
         break;
     }
   }
 
-  modeRoot () {
-    console.log('modeRoot');
-    this.readDirectory()
+  modeRoot() {
+    console.log("modeRoot");
+    this.readDirectory();
   }
 
-  modeMultiple () {
-    console.log('modeMultiple');
+  modeMultiple() {
+    console.log("modeMultiple");
   }
 
   // 读取目录
-  readDirectory () {
-    fs.readdir(this.paths).then(res => {
-      const list = res
-      const filterList: string[] = []
+  readDirectory() {
+    fs.readdir(this.paths)
+      .then((res) => {
+        const list = res;
+        const filterList: string[] = [];
 
-      list.map(item => {
-        if(!item.toLowerCase().includes('index')) {
-          filterList.push(item)
-        }
+        list.map((item) => {
+          if (!item.toLowerCase().includes("index")) {
+            filterList.push(item);
+          }
+        });
+
+        filterList.length !== 0 && this.copyFile(filterList);
       })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
 
-      filterList.length !== 0 && this.fileDataStructure(filterList)
-      
-    }).catch((err) => {
-      console.log(err);
-    })
+  // 复制文件、修改为 cjs 导出
+  copyFile(filterList) {
+    filterList.forEach((e) => {
+      const filePath = path.join(process.cwd(), this.paths, e);
+      const copyPath = "./locales/" + e;
+      fs.copy(filePath, copyPath)
+        .then(() => {
+          const file = fs
+            .readFileSync(copyPath)
+            .toString()
+            .replace("export default", "module.exports = ");
+          fs.outputFile(copyPath, file).then(() => {});
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    });
+    this.fileDataStructure(filterList, path.join(process.cwd(), "./locales/"));
   }
 
   // 处理数据结构
-  fileDataStructure (filterList) {
-    const Rows: string[][]= []
-    const Header: Object[] = [{caption: 'code', type: 'string'}];
-    
-    for(let i = 0; i < filterList.length; i++) {
+  async fileDataStructure(filterList, filePath: string) {
+    for (let i = 0; i < filterList.length; i++) {
       try {
-        // 读取文件、转为Object
-        const file = fs.readFileSync(this.paths + '/' + filterList[i]);
-        const files = file.toString().replace('export default', '')
-        const obj = eval('(' + files + ')');
+        // 读取文件内容
+        const res = await import(path.join(filePath, filterList[i]))
+          .then((res) => {
+            return res;
+          })
+          .catch((err) => {
+            console.log(err);
+          });
 
         // 设置表格头
-        Header.push({caption: filterList[i].split('.')[0], type: 'string'})
+        this.Header.push({
+          caption: filterList[i].split(".")[0],
+          type: "string",
+        });
+        await this.dealNestedData({
+          data: res,
+          langNumber: i,
+          parent: "",
+        });
 
-        // 设置表格行内容
-        Object.keys(obj).forEach((it, id) => {
-
-          if(Rows[id] === undefined) {
-            Rows[id] = []
-          }
-          if(i === 0) {
-            Rows[id].push(it)
-          } 
-          Rows[id].push(obj[it])
-
-        })
+        this.num = 0; // 清空当前词条索引
       } catch (err) {
         console.log(err);
       }
     }
 
-    exportExcelFile(Header, Rows, 'locales')
-    
+    // console.log(this.Rows, this.Header);
+    exportExcelFile(this.Header, this.Rows, "locales");
   }
-  
+
+  // 处理嵌套数据
+  async dealNestedData({ data, langNumber, parent }: deepData) {
+    Object.keys(data).forEach((item, index) => {
+      const d = Object.prototype.toString.call(data[item]);
+      if (d === "[object String]") {
+        if (this.Rows[this.num] === undefined) {
+          this.Rows[this.num] = [];
+        }
+        if (langNumber === 0) {
+          // 词条code
+          this.Rows[this.num].push(parent !== "" ? parent + "." + item : item);
+        }
+        // 词条 value
+        this.Rows[this.num].push(data[item]);
+        this.num += 1; // 词条索引 + 1
+      } else if (d === "[object Object]") {
+        this.dealNestedData({
+          data: data[item],
+          langNumber,
+          parent: parent !== "" ? parent + "." + item : item,
+        });
+      }
+    });
+  }
 }
