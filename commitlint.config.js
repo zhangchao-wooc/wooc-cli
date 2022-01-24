7b0f98de374785404856f162b74524e72c8e6bab
hexie <hexie.zhang@tuya.com>
2022-01-24 14:25:44 +0800

    ci: 校验配置文件修改

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,19 +2,19 @@
 
 module.exports = {
   extends: ['@commitlint/config-conventional'], // 当前 commit 校验使用的标准库（可插拔），内容可见下方
-  rules: {
-    // 这里自定义的配置会覆盖 @commitlint/config-conventional 中的规则
-    // 'scope-enum': [
-    //   // 自定义 范围规则
-    //   2,
-    //   'always',
-    //   ['server', 'client'],
-    // ],
-    // 'type-enum': [
-    //   // 自定义 类型规则
-    //   2,
-    //   'always',
-    //   ['feature'],
-    // ],
-  },
+  // rules: {
+  //   这里自定义的配置会覆盖 @commitlint/config-conventional 中的规则
+  //   'scope-enum': [
+  //     // 自定义 范围规则
+  //     2,
+  //     'always',
+  //     ['server', 'client'],
+  //   ],
+  //   'type-enum': [
+  //     // 自定义 类型规则
+  //     2,
+  //     'always',
+  //     ['feature'],
+  //   ],
+  // },
 };
